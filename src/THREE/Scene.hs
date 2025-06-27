ba588fe54cdaecee1c2cac01ab375537d533d611
dmjio <code@dmj.io>
2025-06-26 23:58:18 -0500

    Drop deriving via, vanilla derive

--- src/THREE/Scene.hs
+++ src/THREE/Scene.hs
@@ -19,31 +19,19 @@ module THREE.Scene
 -----------------------------------------------------------------------------
 import           Language.Javascript.JSaddle
 -----------------------------------------------------------------------------
-import           THREE.Internal as THREE
-import           THREE.Object3D as THREE
+import           THREE.Internal        as THREE
+import           THREE.Object3D        as THREE
+import           THREE.EventDispatcher as THREE
 -----------------------------------------------------------------------------
 -- | https://threejs.org/docs/#api/en/scenes/Scene
 newtype Scene
   = Scene
   { unScene :: JSVal
-  } deriving (MakeArgs, MakeObject, ToJSVal) 
-    deriving Object3D via JSVal
------------------------------------------------------------------------------
--- Constructors
+  } deriving (Object3D, EventDispatcher, MakeArgs, MakeObject, ToJSVal) 
 -----------------------------------------------------------------------------
 new :: THREE.Three Scene
 new = THREE.new Scene "Scene" ()
 -----------------------------------------------------------------------------
--- Read-only properties
------------------------------------------------------------------------------
 isScene :: Property Scene "isScene" Bool
 isScene = property
------------------------------------------------------------------------------
--- Properties
------------------------------------------------------------------------------
--- Optional properties
------------------------------------------------------------------------------
--- Methods
------------------------------------------------------------------------------
---  functionselpers
 -----------------------------------------------------------------------------
