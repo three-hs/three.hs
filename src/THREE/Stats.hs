446292eebab194491c3c9bdb6e955fcd5658001c
Julien Dehos <dehos@univ-littoral.fr>
2025-07-25 11:41:33 +0200

    Stats: fix dom

--- src/THREE/Stats.hs
+++ src/THREE/Stats.hs
@@ -9,27 +9,27 @@ module THREE.Stats
   , THREE.Stats.new
   , showPanel
   , update
-  , begin
-  , end
     -- * Property
   , dom
   ) where
 -----------------------------------------------------------------------------
-import           Language.Javascript.JSaddle hiding (new)
+import           Language.Javascript.JSaddle as J
 -----------------------------------------------------------------------------
 import qualified THREE.Internal as THREE
 -----------------------------------------------------------------------------
 -- | https://threejs.org/docs/#api/en/scenes/Stats
 newtype Stats
   = Stats
-  { unStatsCamera :: JSVal
+  { unStats :: JSVal
   } deriving (MakeObject, ToJSVal)
 -----------------------------------------------------------------------------
 instance FromJSVal Stats where
   fromJSVal = pure . pure . Stats
 -----------------------------------------------------------------------------
-new :: THREE.Three Stats
-new = THREE.new Stats "Stats" ()
+new :: () -> THREE.Three Stats
+new () = do
+  v <- jsg ("Stats" :: JSString)
+  Stats <$> J.new v ()
 -----------------------------------------------------------------------------
 -- | stats.showPanel(1);
 -- 0: FPS, 1: MS, 2: MB, 3+: CUSTOM
@@ -40,12 +40,6 @@ showPanel = THREE.method "showPanel"
 update :: THREE.Method Stats () ()
 update = THREE.method "update"
 -----------------------------------------------------------------------------
-begin :: THREE.Method Stats () ()
-begin = THREE.method "begin"
------------------------------------------------------------------------------
-end :: THREE.Method Stats () ()
-end = THREE.method "end"
------------------------------------------------------------------------------
-dom :: THREE.Property Stats ()
+dom :: THREE.Property Stats JSVal
 dom = THREE.property "dom"
 -----------------------------------------------------------------------------
